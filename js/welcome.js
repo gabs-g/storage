57f8eafc6b3423e90f83de39aefc0524fed899ea
anagabriela <anagarcia.comunicacion@gmail.com>
2025-04-09 16:08:41 -0600

    Se sube el cambio de bienvenido a bienvenid@

--- js/welcome.js
+++ js/welcome.js
@@ -2,9 +2,9 @@ let encabezado1= document.getElementsByTagName("h1").item(0);
 let btnEliminar= document.getElementById("btnEliminar");
 
 
-if (localStorage.getItem("Nombre")){
+if (localStorage.getItem("Nombre")!=null){
 let Nombre = localStorage.getItem("Nombre")
-encabezado1.innerText= `Hola, ${Nombre}, bienvenido de nuevo`;
+encabezado1.innerText= `Hola, ${Nombre}, bienvenid@ de nuevo`;
 console.log(Nombre);
 } else{
     encabezado1.innerText = `Por favor, ve al index e ingresa tu nombre.`
